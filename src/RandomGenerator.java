2bffaab1b77381e0816aaf4ae6367516b6fac210
Chenye Yin <elizebethyin@gmail.com>
2016-03-02 17:15:52 -0500

    Ah

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -1,4 +1,17 @@
+import java.util.Random;
 
-public class RandomGenerator {
+public class RandomGenerator{
+    public static void main(String[] args)
+    {
+        System.out.println("Generate a random integer between 5 and 95");
 
+        Random rnd = new Random();
+        
+        
+          int randomInt = 5 + rnd.nextInt(90);
+          System.out.println("Generated number: " + randomInt);
+        
+    
+        System.out.println("Done.");
+    }
 }
